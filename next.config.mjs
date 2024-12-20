6fbe23b14b8fb193e6d22d819645e413ea174542
Mher <mhergharibyan550@gmail.com>
2024-12-20 22:07:04 +0400

    minor change

--- next.config.mjs
+++ next.config.mjs
@@ -3,27 +3,6 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://casecobra-umber-five.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*", // Apply to all routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://casecobra-umber-five.vercel.app", // Replace with specific origin(s)
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
