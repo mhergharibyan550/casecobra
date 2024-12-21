e610477a4356de51829e994d696583ce734b7a57
Mher <mhergharibyan550@gmail.com>
2024-12-21 21:20:51 +0400

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
 import { constructMetadata } from "@/lib/utils";
+import { AuthProvider } from "@/components/AuthProvider";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
@@ -19,17 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={recursive.className}>
-        <Navbar />
-        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
-          <div className="flex-1 flex flex-col h-full">
-            <Providers>{children}</Providers>
-          </div>
-          <Footer />
-        </main>
-        <Toaster />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={recursive.className}>
+          <Navbar />
+          <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+            <div className="flex-1 flex flex-col h-full">
+              <Providers>{children}</Providers>
+            </div>
+            <Footer />
+          </main>
+          <Toaster />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
