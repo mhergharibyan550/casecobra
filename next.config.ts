a2c0f7bf17fb1f507e4b22bbde5561703129d160
Mher <mhergharibyan550@gmail.com>
2024-12-21 21:28:13 +0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://${process.env.VERCEL_URL}/auth-callback`,
+  },
 };
-
 export default nextConfig;
