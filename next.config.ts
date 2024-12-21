cc642eb4ed75a4cf95e6d0fa4071b67170f8092d
Mher <mhergharibyan550@gmail.com>
2024-12-21 21:10:45 +0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*", // Apply to all API routes
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://casecobra-umber-five.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
