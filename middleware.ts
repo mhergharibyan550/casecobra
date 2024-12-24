75dbc361c5f74c56d759c55d6103b1078f31ecd5
Mher <mhergharibyan550@gmail.com>
2024-12-24 13:05:31 +0400

    Changes

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const allowedOrigins = "*";
-
-const corsOptions = {
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
-
-export function middleware(request: NextRequest) {
-  // const origin = request.headers.get("origin") ?? "";
-  // const isAllowedOrigin = allowedOrigins.includes(origin);
-
-  // const isPreflight = request.method === "OPTIONS";
-
-  // if (isPreflight) {
-  //   const preflightHeaders = {
-  //     ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
-  //     ...corsOptions,
-  //   };
-  //   return NextResponse.json({}, { headers: preflightHeaders });
-  // }
-
+export function middleware(req: NextRequest) {
   const response = NextResponse.next();
-
-  // if (isAllowedOrigin) {
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  // }
-
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
 
   return response;
 }
-
-export const config = {
-  matcher: "/api/auth/:kindeAuth",
-};
