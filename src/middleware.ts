e82a7dc85e4c98696389ec42ff187592f240bda4
Mher <mhergharibyan550@gmail.com>
2025-01-19 15:16:59 +0400

    Chage

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export default function middleware(request: NextRequest) {
     const preflightHeaders = {
       ...{
         "Access-Control-Allow-Origin":
-          "https://casecobra-umber-five.vercel.app/api/auth/logout",
+          "https://casecobra-umber-five.vercel.app/api/auth/login",
       },
       ...corsOptions,
     };
@@ -22,7 +22,7 @@ export default function middleware(request: NextRequest) {
 
   response.headers.set(
     "Access-Control-Allow-Origin",
-    "https://casecobra-umber-five.vercel.app/api/auth/logout"
+    "https://casecobra-umber-five.vercel.app/api/auth/login"
   );
 
   Object.entries(corsOptions).forEach(([key, value]) => {
